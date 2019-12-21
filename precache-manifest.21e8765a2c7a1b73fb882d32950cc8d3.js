c02876312ddd6ae403dce1e09ad64351f72b82f3
unknown <tusharshinde1612>
2019-12-21 18:15:41 +0530

    Updates

---
+++ precache-manifest.21e8765a2c7a1b73fb882d32950cc8d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba80ad5a3433ce05b0211711d5d07a6a",
+    "url": "/Robo_Friend/index.html"
+  },
+  {
+    "revision": "b18025e7f1b5bbc29b83",
+    "url": "/Robo_Friend/static/css/main.4c7de10a.chunk.css"
+  },
+  {
+    "revision": "4ee796cfabedc5bd953d",
+    "url": "/Robo_Friend/static/js/2.cd5ddd3b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Robo_Friend/static/js/2.cd5ddd3b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b18025e7f1b5bbc29b83",
+    "url": "/Robo_Friend/static/js/main.b9c8d230.chunk.js"
+  },
+  {
+    "revision": "7f4cce6d179cd6bc4f58",
+    "url": "/Robo_Friend/static/js/runtime-main.e9feb2f7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Robo_Friend/static/media/logo.5d5d9eef.svg"
+  }
+]);
